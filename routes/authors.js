1d34f678260eaa6aba2ea26b46dfdfb245d6eb3f
alexander "sandy" gilfillan <alexander.l.gilfillan@gmail.com>
2016-04-12 15:32:06 -0700

    print data by index to author and book pages

--- routes/authors.js
+++ routes/authors.js
@@ -4,11 +4,18 @@ var router = express.Router();
 const knex = require('../db/knex');
 
 router.get('/', function(req, res, next) {
-  return knex('authors').select('first', 'last', 'bio', 'url').then(function(author_entry) {
+  return knex('authors').select('first', 'last', 'bio', 'url').then(function(author_entries) {
     return res.render('authors', {
-      author: author_entry
+      authors: author_entries
     })
   })
+})
+
+router.get('/:id', function(req, res, next) {
+  return knex('authors').select('first', 'last', 'bio', 'url').where({id: req.params.id}).then(function(author_entry) {
+      return res.render('author', {
+        author: author_entry});
+  });
 })
 
 module.exports = router;
--- routes/books.js
+++ routes/books.js
@@ -5,7 +5,14 @@ const knex = require('../db/knex');
 
 router.get('/', function(req, res, next) {
   return knex('books').select('title', 'genre', 'description', 'url').then(function(book_entries) {
-      return res.render('books', {title: book_entries});
+      return res.render('books', {titles: book_entries});
+  });
+})
+
+router.get('/:id', function(req, res, next) {
+  return knex('books').select('title', 'genre', 'description', 'url').where({id: req.params.id}).then(function(book_entry) {
+      return res.render('book', {
+        title: book_entry});
   });
 })
 
