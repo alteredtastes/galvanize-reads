bb0287642c83c6465b0b00162cdd3a403541d4e3
alexander "sandy" gilfillan <alexander.l.gilfillan@gmail.com>
2016-04-12 00:03:40 -0700

    configured bootstrap for jade and public/index.html

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+// var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var books = require('./routes/books');
+var authors = require('./routes/authors');
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/books', books);
+app.use('/authors', authors);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
